c2fd50ae1e07a5fe0040f8c58ce4fd29de2953f6
Denis Nikolaev <150382827+NeX0Zz@users.noreply.github.com>
2025-06-30 11:02:43 +0300

    Initial Commit

---
+++ PixVerse_127/PixVerse_127App.swift
@@ -0,0 +1,17 @@
+//
+//  PixVerse_127App.swift
+//  PixVerse_127
+//
+//  Created by Денис Николаев on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct PixVerse_127App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
